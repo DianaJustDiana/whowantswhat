0af3992f1b8915f7e5911eb43df5e658f151feca
Diana Davis <dianadryerdavis@gmail.com>
2018-05-04 15:27:19 -0400

    added static_root

--- mysite/settings.py
+++ mysite/settings.py
@@ -138,6 +138,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
 STATIC_URL = '/static/'
 
 #After user logs in or out, let's redirect to home page.
