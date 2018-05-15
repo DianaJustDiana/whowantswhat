8c99ad1d9678a3f542097f53b93957b6e4331376
Diana Davis <dianadryerdavis@gmail.com>
2018-05-14 23:51:33 -0400

    Added conditions so parent of family group can't add same member twice.

--- family_groups/views.py
+++ family_groups/views.py
@@ -34,7 +34,6 @@ def create_a_family(request):
             form.parent = current_user
             #Then save again for real.
             form.save()
-            #TODO Remove this once the custom template tag is working.
             print(current_user.is_parent)
             #TODO Change this so after user creates a family group, redirect user to add members form.
             return HttpResponseRedirect(reverse('family_groups:add_members'))
@@ -54,40 +53,40 @@ def add_members(request):
         form = AddMembersForm(request.POST)
 
         if form.is_valid():
-             #Saving the form with commit=False generates an object called "form."
+            #Saving the form with commit=False generates an object called "form."
             form = form.save(commit=False)    
             #Here's where I can add extra data and make the object belong to the family group.
             current_user = request.user
-            #print("This is the current user:")
-            #print(current_user)
-            #current_family = Family.objects.get(parent=current_user)
+            print("This is the current user:")
+            print(current_user)
+            current_family = Family.objects.get(parent=current_user)
             #print("This is the current family group:")
             #print(current_family)
             test_this_member = form.name
-            #TODO test this out
-            #print("This should be the current family group, too:")
+            #Get all the members already in this family group.
+            list_of_members = Family.objects.get(parent=current_user)
+            print("This is the list of members:")
+            print(list_of_members)
+            #See if the member the current user wants to add is already in the family group.
+            already_a_member = list_of_members.member_set.filter(name=test_this_member)
+            print("Checking this member:")
+            print(test_this_member)
+            print(already_a_member)
 
-            #TODO Marking this QuerySet so I can find it easily. Important part is member_set.
-            current_family = Family.objects.get(parent=current_user)
-            already_a_member = current_family.member_set.get(name=test_this_member)
-
-            #See if member being added in form already exists in family group.
             if already_a_member:
-                #Tell user they already exist in family group.
+                #TODO Tell user this member already is part of the family group.
                 #flash("I'm sorry, but that username is already taken.", 'error')
                 print("ALREADY A MEMBER OF THIS FAMILY GROUP")
-                #return redirect('/signup')
-
+                return HttpResponseRedirect(reverse('family_groups:add_members'))
             else:
-
+                #Makes existing family group the one the new member is added to.
                 form.family = current_family
                 print(form.family)
                 form = form.save()
                 #After user addes new offer, redirect user to offers index page.
                 return HttpResponseRedirect(reverse('offers:index'))
-            
+
             #TODO end test this out
-    
     context = {'form': form}
     return render(request, 'family_groups/add_members.html', context) 
 
@@ -99,23 +98,22 @@ def index(request):
     current_user = request.user
     #If current user is parent to a family group, this will gather them.
     #TODO Why is this broken? If current user is NOT a parent, this throws an error.
-    list_of_family_groups = Family.objects.filter(parent=current_user)
-    #TODO Remove after testing.
-    print("NOTHING HERE")
-
+    my_family_group = Family.objects.filter(parent=current_user)
     #TODO Marking this QuerySet so I can find it easily. Important part is member_set.
     #Adding condition so if there are no family groups the variable f never enters the picture.
-    if list_of_family_groups:
+    if my_family_group:
         f = Family.objects.get(parent=current_user)
         members = f.member_set.all()
+        family_name = f.family_name
+
     #Need this because next part with context variables needs something for members.
     else:
         members = None
 
     #Context is the dictionary of info that populates the family_groups/index template.
     context = {
-        "title": "My family groups",
-        "family_groups": list_of_family_groups,
+        "title": family_name,
+        "family_groups": my_family_group,
         "members": members,
     }
 
