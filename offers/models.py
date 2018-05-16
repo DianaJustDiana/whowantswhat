b7a8490e333dc02f57fe857ac2f1bf59795f568c
Diana Davis <dianadryerdavis@gmail.com>
2018-05-16 13:55:46 -0400

    Adds comment field to DibComment

--- offers/models.py
+++ offers/models.py
@@ -64,6 +64,7 @@ class DibComment(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     offer = models.ForeignKey(Offer, on_delete=models.CASCADE)
     dib_comment_time_stamp = models.DateTimeField(auto_now_add=True)
+    comment = models.CharField(max_length=255)
 
     
  
