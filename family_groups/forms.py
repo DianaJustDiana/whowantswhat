aa4205a62edc428f742e48b5fceb436ce006a4b3
Diana Davis <dianadryerdavis@gmail.com>
2018-09-16 18:45:12 -0400

    Removed extraneous comments.

--- family_groups/forms.py
+++ family_groups/forms.py
@@ -16,29 +16,17 @@ class AddMembersForm(forms.ModelForm):
 
     class Meta:
         model = Member
-        print("The model part worked.")
         ##fields = "__all__"
         # TODO Need to limit family choices to those where parent = current user.
         fields = ['family', 'name']
         ##labels = {'family': 'choose which family', 'name' : 'add a person by email address'}
 
+    # This section limits dropdown options to stuff owned by the current user.
     def __init__(self, current_user, *args, **kwargs):
-        print("This is from the init")
-        ##current_user = kwargs.pop('current_user')
         super(AddMembersForm, self).__init__(*args, **kwargs)
-        print("This is also from the init")
-
         my_queryset = Family.objects.filter(parent=current_user)
-        print("The current user is parent to these families:")
-        print(my_queryset)
-        # if request.user:
-        ##    queryset = Family.objects.filter(parent=request.user)
-        # else:
-        ##    queryset = Family.objects.all()
-        # Limit the family options to the ones owned by the current user.
         self.fields['family'].queryset = my_queryset
-        print("my_queryset:")
-        print(self.fields['family'].queryset)
+
         # Still working on how to handle member names.
         ##self.fields['name'].queryset = Member.objects.all()
         ##member_name = models.ForeignKey(User, on_delete=models.CASCADE)
